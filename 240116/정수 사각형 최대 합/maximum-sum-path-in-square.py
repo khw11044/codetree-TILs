1e9f73f499a0890022aa87d39abdddf30cb49926
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-16 00:23:49 +0900

    Title: 정수 사각형 최대 합, Time: 195ms, Memory: 25MB, Status: Passed - CodeTree

--- 240116/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240116/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,31 +1,43 @@
-from collections import deque 
+n = int(input())
 
-moves = [(0,1),(1,0)]
+UNUSED = -1
 
-def in_range(nx,ny):
-    return 0<=nx<n and 0<=ny<n
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-if __name__=="__main__":
-    n= int(input())
+memo = [
+    [UNUSED for _ in range(n)]
+    for _ in range(n)
+]
 
-    board = [list(map(int,input().split())) for _ in range(n)]
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-    dp = [[0 for _ in range(n)] for _ in range(n)]
-    visited = [[0]*n for _ in range(n)]
 
-    dp[0][0] = board[0][0]
-    visited[0][0] = 1
-    Q=deque()
-    Q.append((0,n-1))
+def find_max_sum(x, y):
+    # 미리 계산된 적이 있는 경우 해당 값을 사용해줍니다.
+    if memo[x][y] != UNUSED:
+        return memo[x][y]
+    
+    # 도착 지점에 도착하면 최대 합을 갱신해줍니다.
+    if x == n - 1 and y == n - 1:
+        return grid[n - 1][n - 1]
+    
+    dxs, dys = [1, 0], [0, 1]
+    
+    # 가능한 모든 방향에 대해 탐색해줍니다.
+    max_sum = 0 # 주어진 숫자의 범위가 1보다 크기 때문에 항상 갱신됨이 보장됩니다.
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        
+        if in_range(new_x, new_y):
+            max_sum = max(max_sum, find_max_sum(new_x, new_y) + grid[x][y])
+        
+    # 게산된 값을 memo 배열에 저장해줍니다.
+    memo[x][y] = max_sum
+    return max_sum
 
-    while Q:
-        x,y = Q.popleft()
-        for dx,dy in moves:
-            nx=x+dx
-            ny=y+dy
-            if in_range(nx,ny) and visited[nx][ny]<2:
-                visited[nx][ny] +=1
-                dp[nx][ny] = max(dp[nx][ny], dp[x][y]+board[nx][ny])
-                Q.append((nx,ny))
 
-    print(dp[n-1][n-1])
+print(find_max_sum(0, 0))
