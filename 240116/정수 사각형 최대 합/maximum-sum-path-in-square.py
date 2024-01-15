3c5165468117d5d9c51fbc06d4b4b6c70e15932e
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-16 00:24:20 +0900

    Title: 정수 사각형 최대 합, Time: 5089ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240116/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240116/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,26 +1,16 @@
 n = int(input())
 
-UNUSED = -1
-
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-memo = [
-    [UNUSED for _ in range(n)]
-    for _ in range(n)
-]
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 
 def find_max_sum(x, y):
-    # 미리 계산된 적이 있는 경우 해당 값을 사용해줍니다.
-    if memo[x][y] != UNUSED:
-        return memo[x][y]
-    
     # 도착 지점에 도착하면 최대 합을 갱신해줍니다.
     if x == n - 1 and y == n - 1:
         return grid[n - 1][n - 1]
@@ -34,10 +24,7 @@ def find_max_sum(x, y):
         
         if in_range(new_x, new_y):
             max_sum = max(max_sum, find_max_sum(new_x, new_y) + grid[x][y])
-        
-    # 게산된 값을 memo 배열에 저장해줍니다.
-    memo[x][y] = max_sum
+            
     return max_sum
-
-
+                    
 print(find_max_sum(0, 0))
