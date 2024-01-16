45d952e057fc5cac7c6cc2f4bce09c39f683c405
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-16 15:45:42 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 248ms, Memory: 29MB, Status: Passed - CodeTree

--- 240116/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240116/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,71 +1,38 @@
 import sys
 
+def check():
+    d = [[1e9]*n for _ in range(n)]
+    d[0][0] = board[0][0]
 
-def initialize():
-    # 전부 INT_MAX로 초기화합니다.
-    for i in range(n):
-        for j in range(n):
-            dp[i][j] = INT_MAX
-
-    # 시작점의 경우 dp[0][0] = board[0][0]으로 초기값을 설정해줍니다
-    dp[0][0] = board[0][0]
-
-    # 최좌측 열의 초기값을 설정해줍니다.
-    for i in range(1, n):
-        dp[i][0] = max(dp[i - 1][0], board[i][0])
-
-    # 최상단 행의 초기값을 설정해줍니다.
     for j in range(1, n):
-        dp[0][j] = max(dp[0][j - 1], board[0][j])
-
-
-def solve(lower_bound):
-    # lower_bound 미만의 값은 사용할 수 없도록
-    # board값을 변경해줍니다.
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] < lower_bound:
-                board[i][j] = INT_MAX
+        d[0][j] = max(d[0][j-1], board[0][j])
     
-    # DP 초기값 설정
-    initialize()
+    for i in range(1, n):
+        d[i][0] = max(d[i-1][0], board[i][0])
 
-    # 탐색하는 위치의 위에 값과 좌측 값 중에 작은 값과
-    # 해당 위치의 숫자 중에 최댓값을 구해줍니다.
     for i in range(1, n):
         for j in range(1, n):
-            dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), board[i][j])
-        
-    return dp[n - 1][n - 1]
+            d[i][j] = max(min(d[i-1][j], d[i][j-1]), board[i][j])
+    
+    return d[n-1][n-1]  # 1e9면 경로 없음!
 
 
 if __name__=="__main__":
 
-    INT_MAX = sys.maxsize
-    MAX_R = 100
-
-    # 변수 선언 및 입력:
     n = int(input())
     board = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[0] * n for _ in range(n)]
 
-    ans = INT_MAX
+    INT_MAX=sys.maxsize
 
-   
-    # 최솟값을 k라고 가정했을 때
-    # lower_bound 이상의 수들만 사용하여 
-    # 이동한다는 조건하에서
-    # 최댓값을 최소로 만드는 DP 문제를 풀어줍니다.
-    for lower_bound in range(1, MAX_R + 1):
-        upper_bound = solve(lower_bound)
-        
-        # 다 진행했음에도 여전히 INT_MAX라면 
-        # 그러한 이동이 불가능하다는 뜻이므로
-        # 패스합니다.
-        if upper_bound == INT_MAX:
-            continue
-        
-        # 답을 갱신합니다.
-        ans = min(ans, (upper_bound - lower_bound))
+    res = INT_MAX  # |최대-최소|의 최소!
+    for low in range(1, 101):
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] < low:
+                    board[i][j] = INT_MAX
 
-    print(ans)
+        ans = check()
+        if ans < INT_MAX:
+            res = min(res, abs(ans-low))
+
+    print(res)
