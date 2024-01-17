0368101da8cd24cf0f15c94c08c85083fc341a00
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-17 19:06:10 +0900

    Title: 트리 노드 제거, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240117/트리 노드 제거/remove-tree-node.py
@@ -0,0 +1,44 @@
+# DFS를 통해 x 아래의 리프노드의 개수를 찾아줍니다.
+def DFS(x):
+    global ans
+    
+    if is_deleted[x]:   # (루트 노드가 삭제되었을 때) 삭제된 노드는 스킵합니다.
+        return
+
+    # x번 노드가 리프노드인지 판단합니다. 자신의 자식 노드가
+    # 하나라도 남아 있을 경우 x번 노드는 리프노드가 아닙니다.
+    is_leaf = True
+    if len(graph[x]) != 0:      # 연결된 자식 노드가 하나라도 있으면 
+        for y in graph[x]:
+            # 삭제된 노드는 스킵합니다.
+            if is_deleted[y]: 
+                continue
+            DFS(y)
+
+            is_leaf = False
+    else:       # 연결된 자식 노드가 없으면 즉, 리프노드이면 
+        if is_leaf: 
+            ans += 1
+
+if __name__=="__main__":
+    n = int(input())
+    par = list(map(int, input().split()))   # n개의 간선 정보를 입력받습니다.
+    target = int(input())   # 제거할 노드 
+
+    root = -1
+    graph = [[] for _ in range(n)]
+    is_deleted = [False] * (n)
+
+    for x,y in enumerate(par):
+        if y == -1:         # -1이 입력된 경우 루트 노드로 지정합니다.
+            root = x        # root는 0번째 노드 
+            continue
+        graph[y].append(x)  # 간선 정보를 인접리스트에 넣어줍니다.
+
+    # 노드를 제거하는 방법에는 간선을 직접 끊어주는 등 여러 방법이 있습니다.
+    # 여기서는 삭제되었는지 여부를 배열로 관리합니다.
+    is_deleted[target] = True   # 일단 제거할 노드부터 삭제 
+
+    ans = 0
+    DFS(root)   # root 정점으로부터 리프노드의 개수를 찾습니다.
+    print(ans)
