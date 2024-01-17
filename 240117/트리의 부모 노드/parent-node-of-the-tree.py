26e04303f72c8bb9bd29d57a9f61b10620629bcb
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-17 16:06:01 +0900

    Title: 트리의 부모 노드, Time: 310ms, Memory: 30MB, Status: Passed - CodeTree

--- 240117/트리의 부모 노드/parent-node-of-the-tree.py
+++ 240117/트리의 부모 노드/parent-node-of-the-tree.py
@@ -6,14 +6,8 @@ if __name__=="__main__":
 
     for _ in range(n-1):
         a,b=map(int, input().split())
-        graph[a].append(b)
-        # graph[b].append(a)
+        # graph[a].append(b)
+        graph[b].append(a)
     
-    Q = deque()
-    Q.append(1)
-
-    while Q:
-        x = Q.popleft()
-        for e in graph[x]:
-            print(x)
-            Q.append(e)
+    for i in range(2,n+1):
+        print(graph[i][0])
