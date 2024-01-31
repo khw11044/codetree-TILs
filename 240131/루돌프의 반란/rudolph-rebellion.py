7f893a090b99b7971a0532fe45d4a202a195fb76
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-01-31 16:25:08 +0900

    Title: 루돌프의 반란, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/루돌프의 반란/rudolph-rebellion.py
@@ -0,0 +1,199 @@
+import sys
+
+dxs = [0,1,1,1,0,-1,-1,-1]
+dys = [1,1,0,-1,-1,-1,0,1]
+
+sdxs = [-1,0,1,0]
+sdys = [0,1,0,-1]
+
+def select_santa(cow):
+    r1,c1=cow
+    close_dis=2500
+    select_santa_num=0
+    select_santa_loc=[0,0]
+    for i in range(1,P+1):
+        r2,c2 = santa[i]
+        if r2==-1 and c2==-1:               # 이미 경기장 밖으로 나감 
+            continue
+        dis = (r1-r2)**2 + (c1-c2)**2
+        if dis <= close_dis:
+            if r2>select_santa_loc[0]:
+                select_santa_num=i
+                select_santa_loc=[r2,c2]
+                close_dis = dis
+            elif r2==select_santa_loc[0]:
+                if c2>select_santa_loc[1]:
+                    select_santa_num=i
+                    select_santa_loc=[r2,c2]
+                    close_dis = dis
+                    
+    return select_santa_num, select_santa_loc
+
+def Interaction(crush_new_santa_num,dict_x,dict_y):
+    
+    x,y = santa[crush_new_santa_num]
+    new_x = x + dict_x
+    new_y = y + dict_y
+    
+    if not in_range(new_x,new_y):
+        santa[crush_new_santa_num] = [-1,-1]      # 보드 밖으로 나감
+    else:
+        TF, p_num = check(new_x,new_y)              # 새 위치에 산타가 있는지 없는지 확인 
+        if not TF:                                  # 있는경우 상호작용 
+            Interaction(p_num,dict_x,dict_y)
+        else:
+            santa[crush_new_santa_num] = [new_x,new_y]
+            
+        
+
+def cow_crash(cow_loc,x1,y1,p_num,x2,y2,k):
+    santa_score[p_num] += C
+    dict_x = x1-cow_loc[0]
+    dict_y = y1-cow_loc[1]
+    
+    x2 += dict_x*C
+    y2 += dict_y*C
+    
+    if not in_range(x2,y2):
+        santa[p_num] = [-1,-1]      # 보드 밖으로 나감 
+    else:
+        faint[p_num] = 2 # k+1            # 기절  # 두턴뒤에 풀림 
+        TF,crush_new_santa_num = check(x2,y2)
+        if not TF:
+            # 상호작용 crush_new_santa_num
+            Interaction(crush_new_santa_num,dict_x,dict_y)
+
+        santa[p_num] = [x2,y2]  # 상호작용이랑 상관없이 이동할건 해야지
+        
+
+def santa_crash(p_num,x1,y1,k):
+    santa_score[p_num] += D
+    dict_x = santa[p_num][0] - x1
+    dict_y = santa[p_num][1] - y1
+    
+    x1 += dict_x*D
+    y1 += dict_y*D
+    
+    if not in_range(x1,y1):
+        santa[p_num] = [-1,-1]      # 보드 밖으로 나감 
+    else:
+        faint[p_num] = 1 # k+1            # 기절  # 두턴뒤에 풀림 
+        TF,crush_new_santa_num = check(x1,y1)
+        if not TF:
+            # 상호작용 crush_new_santa_num
+            Interaction(crush_new_santa_num,dict_x,dict_y)
+            
+        santa[p_num] = [x1,y1]   # 상호작용이랑 상관없이 이동할건 해야지
+        
+
+def in_range(x,y):
+    return 0<x<=N and 0<y<=N
+
+def check(x1,y1):
+    for i in range(1,P+1):
+        if santa[i] == [x1,y1]:
+            return False, i
+    return True, 0
+
+def santa_move_rule(x1,y1,x2,y2):
+    dis = (x1-x2)**2 + (y1-y2)**2
+    new_x,new_y=x1,y1
+    for dx,dy in zip(sdxs,sdys):
+        nx=x1+dx
+        ny=y1+dy
+        
+        if in_range(nx,ny):
+            TF,_ = check(nx,ny)
+            if TF:
+                new_dis = (nx-x2)**2 + (ny-y2)**2
+                if new_dis < dis:
+                    dis = new_dis
+                    new_x,new_y=nx,ny
+    return new_x,new_y                
+
+def cow_move_rule(x1,y1,x2,y2):
+    if x1>x2:
+        x1 -= 1
+    elif x1<x2:
+        x1 += 1
+    else:
+        x1 = x1 
+    
+    if y1>y2:
+        y1 -= 1
+    elif y1<y2:
+        y1 += 1
+    else:
+        y1 = y1
+    
+    return x1,y1    
+
+# 루돌프 움직임 
+def cow_move(cow_loc,k):
+    x1,y1 = cow_loc
+    p_num, [x2,y2] = select_santa(cow_loc)
+    x1,y1 = cow_move_rule(x1,y1,x2,y2)
+
+    # 만약 루돌프가 산타랑 충돌할때 
+    if [x1,y1]==[x2,y2]:
+        cow_crash(cow_loc,x1,y1,p_num,x2,y2,k)
+    
+    return [x1,y1]
+
+
+# 산타 움직임 
+def santa_move(cow_loc,k):
+    x2,y2=cow_loc
+    cnt = 0
+    for i in range(1,P+1):
+        if santa[i] == [-1,-1]:  # 탈락한 산타는 움직임 없음 
+            cnt += 1
+            continue
+        
+        if faint[i]!=0:          # 기절한 산타는 움직임 없음 
+            faint[i]-=1
+            continue
+        
+        x1,y1=santa[i]
+        
+        x1,y1 = santa_move_rule(x1,y1,x2,y2)
+        
+        # 산타가 움직였는데 루돌프랑 부딪칠 경우 
+        if [x1,y1] == [x2,y2]:
+            santa_crash(i,x1,y1,k)
+        else:
+            santa[i]=[x1,y1]
+        
+    return cnt    
+        
+
+if __name__=="__main__":
+    N,M,P,C,D = map(int, input().split())
+    board = [[0]*(N+1) for _ in range(N+1)]
+    cow_loc = list(map(int, input().split()))   # 소의 위치 (Rr, Rc)
+    santa = {}
+    for _ in range(P):
+        loc,x,y=map(int, input().split())
+        santa[loc]=[x,y]
+    
+
+    # 기절 상태 기록 
+    faint = [0]*(P+1)
+    santa_score = [0]*(P+1)
+
+    total_score = 0
+    # 게임 플레이수 M
+    for k in range(1,M+1):
+        # 루돌프 움직이고 
+        cow_loc = cow_move(cow_loc,k)
+
+        # 1번 산타부터 P번산타까지 산타들 움직임 
+        cnt = santa_move(cow_loc,k)
+        if cnt == P:
+            break
+        
+        for i in range(1,P+1):
+            if santa[i] != [-1,-1]:
+                santa_score[i] += 1
+
+    print(*santa_score[1:])
