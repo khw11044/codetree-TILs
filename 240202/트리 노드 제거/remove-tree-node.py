ff86a6a5fc9198d29d132f2c15d7f020c9a2d081
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-02 08:37:32 +0900

    Title: 트리 노드 제거, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240202/트리 노드 제거/remove-tree-node.py
@@ -0,0 +1,41 @@
+import sys
+
+def DFS(x):
+    global ans 
+    
+    if is_deleted[x]:
+        return
+    
+    is_leaf=True
+    if len(graph[x]) != 0:
+        is_leaf=False
+        for y in graph[x]:
+            if is_deleted[y]:
+                continue
+            DFS(y)
+            
+    if is_leaf:
+        ans += 1
+
+
+if __name__=="__main__":
+    n=int(input())
+    par = list(map(int,input().split()))
+    
+    graph = [[] for _ in range(n+1)]
+    is_deleted = [False]*n
+    root = -1
+    for x,y in enumerate(par):
+        if y==-1:
+            root=x
+            continue
+        
+        graph[y].append(x)
+    
+    target = int(input())
+    
+    is_deleted[target]=True 
+    
+    ans=0
+    DFS(root)
+    print(ans)
