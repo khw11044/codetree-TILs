68258abab3df53e93a919669c421b7be9e3f47d1
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-02 11:49:08 +0900

    Title: 트리 사촌, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240202/트리 사촌/beard-tree.py
+++ 240202/트리 사촌/beard-tree.py
@@ -1,38 +1,35 @@
 import sys
 
-def DFS(x):
-    for y in graph[x]:
-        if visited[y]:
-            continue
-        visited[y]=True
-        depth[y] = depth[x]+1
-        DFS(y)
-
 if __name__=="__main__":
-    n,k=map(int, input().split())
-    nodes = list(map(int, input().split()))
-    root = nodes[0]
-    max_size = 1_000_000
-    graph = [[] for _ in range(max_size+1)]
-    visited = [False]*(max_size+1)
-    cnt=0
     
-    for i in range(1, len(nodes)-1):
-        if nodes[i]+1 == nodes[i+1]:
-            graph[nodes[cnt]].append(nodes[i])
+    # 변수 선언 및 입력:
+    n, k = tuple(map(int, input().split()))
+    par = [0] * (n + 1)
+    finding_node_index = -1
+    ans = 0
 
-        else:
-            graph[nodes[cnt]].append(nodes[i])
-            cnt += 1
-    
-    depth = [0]*(max_size+1)
-    visited[root]=True
-    DFS(root)
-    ans=0
-    for i in range(1, len(nodes)):
-        if i == k:
+    # n개의 수열 원소를 입력받습니다.
+    nodes = list(map(int, input().split()))
+    for i in range(1, n):
+        if nodes[i] == k: 
+            finding_node_index = i
+
+    # n개의 원소를 바탕으로 트리를 만들어줍니다.
+    parent_node = 0
+    for i in range(1, n):
+        if nodes[i] > nodes[i-1]+1:
+            parent_node += 1
+        
+        par[i] = parent_node
+
+    for cur_node in range(1, n):
+        # cur_node의 부모의 부모 노드가 존재하지 않는 경우에는 패스
+        if not par[par[cur_node]] or not par[par[finding_node_index]]:
             continue
-        if depth[nodes[i]] == depth[k]:
-           ans += 1 
-    
+        
+        # 부모 노드는 다르면서 and 부모의 부모 노드가 같은 경우를 찾습니다.
+        if par[cur_node] != par[finding_node_index] and par[par[cur_node]] == par[par[finding_node_index]]:
+            ans += 1    # 해당 노드들은 사촌이 됩니다.
+
+    # 사촌 노드의 개수를 출력합니다.
     print(ans)
