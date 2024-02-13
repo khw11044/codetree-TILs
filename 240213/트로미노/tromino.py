267d2c33c1c773c6e53a789040c31634099cb96e
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-13 16:22:08 +0900

    Title: 트로미노, Time: 242ms, Memory: 26MB, Status: Passed - CodeTree

--- 240213/트로미노/tromino.py
+++ 240213/트로미노/tromino.py
@@ -1,26 +1,39 @@
 import sys
-def check(r,c):
-    ans11, ans12, ans13, ans21, ans22 = 0,0,0,0,0
+
+def check1(r,c):
+    ans11, ans12, ans13, ans14 = 0,0,0,0
     # 1번 블록의 1번자세 
-    if r+1<n and c+1<m:
-        ans11 = board[r][c] + board[r+1][c] + board[r+1][c+1]
+    if 0<=r-1 and r<n and 0<=c and c+1<m:
+        ans11 = board[r-1][c] + board[r][c] + board[r][c+1]
     
     # 1번 블록의 2번자세 
-    if r+1<n and c+1<m:
-        ans12 = board[r][c] + board[r+1][c] + board[r][c+1]
+    if 0<=r and r+1<n and 0<=c and c+1<m:
+        ans12 = board[r][c] + board[r][c+1] + board[r+1][c]
     
     # 1번 블록의 3번자세 
-    if r+1<n and c+1<m:
-        ans13 = board[r][c] + board[r][c+1] + board[r+1][c+1]
+    if 0<=r and r+1<n and 0<=c-1 and c<m:
+        ans13 = board[r][c-1] + board[r][c] + board[r+1][c]
+        
+    # 1번 블록의 4번자세 
+    if 0<=r-1 and r<n and 0<=c-1 and c<m:
+        ans14 = board[r-1][c] + board[r][c] + board[r][c-1]
+        
     
+    return max([ans11,ans12,ans13,ans14])
+
+def check2(r,c):
+    ans21, ans22 = 0,0
     # 2번 블록의 1번자세 
-    if c+2<m:
+    if c+3<=m:
         ans21 = sum(board[r][c:c+3])
+    
     # 2번 블록의 2번자세 
-    if r+2<n:
+    if r+3<=n:
         ans22 = sum(list(row[c] for row in board[r:r+3]))
     
-    return max([ans11,ans12,ans13,ans21,ans22])
+        
+    
+    return max([ans21,ans22])
 
 if __name__=="__main__":
     n,m=map(int, input().split())   # n행 m열
@@ -29,6 +42,6 @@ if __name__=="__main__":
     answer = 0
     for i in range(n):
         for j in range(m):
-            answer = max(answer, check(i,j))
+            answer = max(answer, check1(i,j), check2(i,j))
     
     print(answer)
