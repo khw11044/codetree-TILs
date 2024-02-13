3e1363ecdacc32279a9007427f48b36cde093503
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-13 11:54:59 +0900

    Title: 왕실의 기사 대결, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240213/왕실의 기사 대결/royal-knight-duel.py
+++ 240213/왕실의 기사 대결/royal-knight-duel.py
@@ -1,97 +1,106 @@
-import sys
-
 from collections import deque
+
+import sys
 
 moves = [(-1,0),(0,1),(1,0),(0,-1)]
 
-
+# 범위 
 def in_range(nx,ny,h,w):
     return 1<=nx<=L+1-h and 1<=ny<=L+1-w
 
-def try_move(pid,d):
-    KQ=deque()
-    KQ.append(pid)
-    is_moved[pid]=True 
-    
-    # 초기화 단계 
-    for i in range(1,N+1):
-        dmg[i]=0
-        is_moved[i]=False
-        new_r[i]=r[i]
-        new_c[i]=c[i]
-    
-    while KQ:
-        x=KQ.popleft()
-        new_r[x] += moves[d][0]
-        new_c[x] += moves[d][1]
-        
-        # 범위 넘어가는지 확인 
-        if not in_range(new_r[x],new_c[x],h[x],w[x]):
-            return False
-        
+# 움직임을 시도해봅니다.
+def try_movement(idx, dir):
+    q = deque()
+    q.append(idx)
+    is_moved[idx] = True            # 현재 기사를 기준으로 움직임 시작 
+
+    # 초기화 작업입니다.
+    for pid in range(1, N + 1):     # 1번 기사부터 N번 기사까지 
+        dmg[pid] = 0                # 받은 데미지 0으로 초기화 
+        is_moved[pid] = False       # 움직임 False로 초기화 
+        nr[pid] = r[pid]            # 새로운 위치 r을 일단 현재 위치 r로 초기화
+        nc[pid] = c[pid]            # 새로운 위치 c을 일단 현재 위치 c로 초기화
+
+    while q:
+        x = q.popleft()
+        nr[x] += moves[dir][0]            # 새로운, 움직일 위치 
+        nc[x] += moves[dir][1]
+
+        # 경계를 벗어나는지 체크합니다.
+        if not in_range(nr[x],nc[x],h[x],w[x]):
+            return False        # 범위를 벗어나는 움직임이면 못 움직임
+
         # 함정이거나 벽인지 확인 
-        for i in range(new_r[x],new_r[x]+h[x]):
-            for j in range(new_c[x],new_c[x]+w[x]):
-                if board[i][j]==1:  # 함정이면 
-                    dmg[x]+=1
-                if board[i][j]==2:  # 벽이면 
-                    return False
-        
-        # 다른 기사와 충돌하는지 확인 
-        for idx in range(1,N+1):
-            if is_moved[idx] or k[idx]<=0:  # 체력없는 pid 기사는 안움직임 
-                continue
+        for i in range(nr[x], nr[x] + h[x]):
+            for j in range(nc[x], nc[x] + w[x]):
+                if info[i][j] == 1:     # 함정이면 
+                    dmg[x] += 1         # 데미지 축적 
+                if info[i][j] == 2:     # 벽이 하나라도 있으면 
+                    return False        # 못 움직임 
+
+        # 대상 기사가 다른 기사와 충돌하는 경우, 해당 조각도 같이 이동합니다.
+        for pid in range(1, N + 1):
+            if is_moved[pid] or k[pid] <= 0:    # 이미 움직였거나, 체력이 0이하라 체스판에 없는 경우 
+                continue                        # 안움직임 
             # 다른기사의 위치가 현재 기사의 새로운 위치 범위보다 멀면, 
             # 또는 현재 기사의 새로운 위치가 다른기사 범위 밖에 있으면, 즉 연쇄반응이 안일어나면 
-            if (r[idx]>new_r[x]+h[x]-1) or (new_r[x] > r[idx]+h[idx]-1):
+            if r[pid] > nr[x] + h[x] - 1 or nr[x] > r[pid] + h[pid] - 1:
                 continue
-            if (c[idx]>new_c[x]+w[x]-1) or (new_c[x] > c[idx]+w[idx]-1):
+            if c[pid] > nc[x] + w[x] - 1 or nc[x] > c[pid] + w[pid] - 1:
                 continue
-            
-            is_moved[idx]=True 
-            KQ.append(idx)
-    
-    dmg[pid]=0
-    return True
-            
 
-def move(pid,d):
-    if k[pid]<=0:
-        return
-    
-    if try_move(pid, d):
-        for idx in range(1,N+1):
-            r[idx]=new_r[idx]
-            c[idx]=new_c[idx]
-            k[idx] -= dmg[idx]
-        
+            is_moved[pid] = True
+            # 연쇄적으로 움직이게 하기 위해 큐를 사용 
+            q.append(pid)       # 그 다음 움직여야 할 기사 pid         
+
+    # return False 없이 여기까지 무사히 도달했으면 
+    dmg[idx] = 0        
+    return True
+
+
+# 특정 조각을 지정된 방향으로 이동시키는 함수입니다.
+def move_piece(idx, move_dir):
+    if k[idx] <= 0:             # 체력이 0이하라 체스판에 없는 경우 
+        return                  # 끝 
+
+    # 이동이 가능한 경우,
+    if try_movement(idx, move_dir): # idx 기사가 움직일 수 있는지 확인 
+        for pid in range(1, N + 1): # 기사들의 실제 위치와 체력을 업데이트한다.
+            r[pid] = nr[pid]
+            c[pid] = nc[pid]
+            k[pid] -= dmg[pid]
+
 
 if __name__=="__main__":
-    L,N,Q=map(int, input().split())
-    board = [[0]*(L+2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(L)] + [[0]*(L+2)]
+    # L:체스판의 크기, N:기사의 수, Q:명령의 수 
+    L, N, Q = map(int, input().split())
     MAX_N = 31  # 최대 기사 수 
     MAX_L = 41  # 최대 체스판 크기 
+    info = [[0 for _ in range(MAX_L)] for _ in range(MAX_L)]    # 최대크기 체스판
+    for i in range(1, L + 1):
+        info[i][1:] = map(int, input().split())
+        
+    init_k = [0 for _ in range(MAX_N)]   # 최대 개수 기사들의 초기 체력  
+    r = [0]*MAX_N      # 처음 기사 위치 행 
+    c = [0]*MAX_N       # 처음 기사 위치 열 
+    h = [0]*MAX_N      # 기사의 범위 세로 h 
+    w = [0]*MAX_N       # 기사의 범위 가로 w 
+    k = [0]*MAX_N       # 기사의 체력 
+    nr = [0]*MAX_N      # 기사가 움직일 위치 행 
+    nc = [0]*MAX_N      # 기사가 움질일 위치 열 
+    dmg = [0]*MAX_N     # 기사가 받은 데미지 
+    is_moved = [False]*MAX_N    # 움직임 체크 
     
-    r=[0]*31    # 기사 위치 행 
-    c=[0]*31    # 기사 위치 열 
-    h=[0]*31    # 기사 범위 세로
-    w=[0]*31    # 기사 범위 가로 
-    k=[0]*31    # 초기 체력 k 
-    new_r=[0]*31
-    new_c=[0]*31
-    dmg=[0]*31
-    is_moved=[False]*31
-    init_k = [0]*31
-    
-    for pid in range(1,N+1):
-        r[pid],c[pid],h[pid],w[pid],k[pid]=map(int, input().split())
-        init_k[pid]=k[pid]
-    
-    # 왕의 명령 
+    # 기사 번호에 따른 각각의 정보를 리스트에 담는다?
+    for pid in range(1, N + 1):
+        r[pid], c[pid], h[pid], w[pid], k[pid] = map(int, input().split())
+        init_k[pid] = k[pid]
+
+    # Q개의 왕의 명령 
     for _ in range(Q):
-        i,d=map(int, input().split())   # i번 기사에게 방향 d 
-        move(i,d)
-    
-    # 생존한 기사들이 총 받은 대미지의 합을 출력
-    ans = sum([init_k[i] - k[i] for i in range(1,N+1) if k[i]>0])
+        idx, d = map(int, input().split())  # i번의 기사에게 방향 d로 한칸 이동하라는 명령 
+        move_piece(idx, d)
+
+    # 결과를 계산하고 출력합니다.
+    ans = sum([init_k[i] - k[i] for i in range(1, N + 1) if k[i] > 0])
     print(ans)
