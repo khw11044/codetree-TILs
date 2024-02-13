80277cbd106a04e84cc5270de55e5765f3fa561f
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-13 15:24:20 +0900

    Title: 행복한 수열의 개수, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240213/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240213/행복한 수열의 개수/number-of-happy-sequence.py
@@ -9,13 +9,13 @@ if __name__=="__main__":
         row = list(map(int, input().split()))
         board.append(row)
         for i in range(n-m+1):
-            if sum(row[i:i+m]) == row[i]*m:
+            if row[i:i+m].count(row[i]) == m:
                 answer+=1
                 break
                 
     for cow in zip(*board):
         for i in range(n-m+1):
-            if sum(cow[i:i+m]) == cow[i]*m:
+            if cow[i:i+m].count(cow[i]) == m:
                 answer+=1
                 break
             
