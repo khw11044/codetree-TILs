aefa8fe1b835d249407242c046c45d3d0854cc3a
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-14 22:35:35 +0900

    Title: 금 채굴하기, Time: 342ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/금 채굴하기/gold-mining.py
+++ 240214/금 채굴하기/gold-mining.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def in_range(r,c):
     return 0<=r<n and 0<=c<n
 
@@ -16,6 +15,7 @@ def filter(x,y):
                     cnt+=1
                 if in_range(x+(k-i),j) and board[x+(k-i)][j]==1:
                     cnt+=1
+                    
         # 마름모 가운데
         for l in range(y-k,y+k+1):
             if in_range(x,l) and board[x][l]==1:
@@ -27,14 +27,16 @@ def filter(x,y):
         if profit>=0:
             ans = max(ans, cnt)
 
+
     return ans
 
 if __name__=="__main__":
     n,m=map(int, input().split())   # nxn,  m:금 하나의 가격 
     board = [list(map(int, input().split())) for _ in range(n)]
+    
     # n이 3일때, k는 최대 2
     # n이 5일때, k는 최대 4
-    max_k=n-1+1
+    max_k=n+1
     answer = 0
     
     for i in range(n):
