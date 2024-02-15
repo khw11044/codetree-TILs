575c3fc6bf50d0349a5da81065198c6c49646cb2
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-15 17:19:30 +0900

    Title: 삼각형 컨베이어 벨트, Time: 178ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240215/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,19 @@
+import sys
+
+
+from collections import deque
+
+
+if __name__=="__main__":
+    n,t=map(int, input().split())   # n:벨트길이, t=t초후 
+    first = list(map(int, input().split()))
+    second = list(map(int, input().split()))
+    third = list(map(int, input().split()))
+    
+    total_list = deque(first + second + third)
+    total_list.rotate(t)
+    total_list = list(total_list)
+    
+    print(*total_list[:3])
+    print(*total_list[3:6])
+    print(*total_list[6:])
