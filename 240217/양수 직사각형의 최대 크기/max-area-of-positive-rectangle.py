c63d18847591d1286f578d14b38d8bc506832ef2
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-17 14:26:15 +0900

    Title: 양수 직사각형의 최대 크기, Time: 252ms, Memory: 28MB, Status: Passed - CodeTree

--- 240217/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240217/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,14 +1,8 @@
 import sys
 
-
+                
 def check_all_positive(x1,y1,x2,y2):
-    ans = 0
-    for i in range(x1,x2+1):
-        for j in range(y1,y2+1):
-            if board[i][j]<=0:
-                return 0
-            ans += board[i][j]
-    return ans
+    return all(board[i][j]>0 for i in range(x1,x2+1) for j in range(y1,y2+1))
                 
 
 if __name__=="__main__":
@@ -16,15 +10,15 @@ if __name__=="__main__":
     board = [list(map(int, input().split())) for _ in range(n)]
     INT_MIN = -sys.maxsize
     
-    max_area = INT_MIN
+    max_area = -1
     for i in range(n):
         for j in range(m):
             for k in range(i,n):
                 for l in range(j,m):
-                    tmp = check_all_positive(i,j,k,l)
-                    if tmp == 0:
+                    if check_all_positive(i,j,k,l):
+                        max_area = max(max_area, (k-i+1)*(l-j+1))
+                    else:
                         break
-                    max_area = max(max_area, (k-i+1)*(l-j+1))
 
             
     print(max_area)
