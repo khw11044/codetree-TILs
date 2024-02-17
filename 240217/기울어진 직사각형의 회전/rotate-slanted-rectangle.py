a54d566d05b655c1c9f00f76f3b6fa3d6112079d
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-17 19:22:54 +0900

    Title: 기울어진 직사각형의 회전, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240217/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240217/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,12 +1,14 @@
 import sys
 
-def shift(x, y, k, l, move_dir):
+def shift(x, y, w, h, move_dir):
     if move_dir == 0:
-        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
-        move_nums = [k, l, k, l]
+        dxs = [-1, -1, 1, 1]
+        dys = [1, -1, -1, 1]
+        move_nums = [w, h, w, h]
     else:
-        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
-        move_nums = [l, k, l, k]
+        dxs = [-1, -1, 1, 1]
+        dys = [-1, 1, 1, -1]
+        move_nums = [h, w, h, w]
     
     # Step1. temp 배열에 board 값을 복사합니다.
     for i in range(n):
@@ -16,9 +18,11 @@ def shift(x, y, k, l, move_dir):
     # Step2. 기울어진 직사각형의 경계를 쭉 따라가면서 shift
     for dx, dy, move_num in zip(dxs, dys, move_nums):
         for _ in range(move_num):
-            nx, ny = x + dx, y + dy
+            nx = x + dx
+            ny = y + dy
             temp[nx][ny] = board[x][y]
-            x, y = nx, ny
+            x = nx 
+            y = ny
     
     # Step3. temp값을 board에 옮겨줍니다.
     for i in range(n):
@@ -34,7 +38,7 @@ if __name__=="__main__":
 
 
     x, y, m1, m2, m3, m4, d = tuple(map(int, input().split()))
-    shift(x - 1, y - 1, m1, m2, d)
+    shift(x-1, y-1, m1, m2, d)
 
     for b in board:
         print(*b)
