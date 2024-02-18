8e5a68a88fe905d9667df76af336016bba8e51a7
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-18 14:07:36 +0900

    Title: 최단 Run Length 인코딩, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240218/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,33 @@
+import sys
+
+from collections import deque
+
+def get_length(arr):
+    arr = list(arr)
+    new_arr = ''
+    cnt=1
+    for i in range(N-1):
+        if arr[i] == arr[i+1]:
+            cnt += 1
+        else:
+            new_arr += arr[i] + str(cnt)
+            cnt=1
+        
+        if i+1 == N-1:
+            new_arr += arr[i] + str(cnt)
+            
+    return len(new_arr)
+    
+
+if __name__=="__main__":
+    strings = list(map(str, input()))
+    N = len(strings)
+    strings = deque(strings)
+    
+    init_length = get_length(strings)
+    
+    for i in range(1,N):
+        strings.rotate(i)
+        init_length = min(init_length, get_length(strings))
+        
+    print(init_length)
