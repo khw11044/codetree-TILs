8e33b59f1b8e9681ca16732de6e9a2ed3d7a9536
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-22 16:11:09 +0900

    Title: 1차원 폭발 게임, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240222/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240222/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,18 +1,39 @@
 import sys
 
-
 if __name__=="__main__":
     N,M=map(int, input().split())
     arr=[]
     for i in range(N):
         boom=int(input())
-        if arr:
-            if arr[-1] == boom:
-                arr.pop()
+        arr.append(boom)
+    
+    while len(arr)>1:
+        cnt=0
+        delete_list=[]
+        for i in range(len(arr)-1):
+            if arr[i]==arr[i+1]:
+                cnt+=1
             else:
-                arr.append(boom)
-        else:
-            arr.append(boom)
+                if cnt>=M-1:
+                    delete_list += list(range(i-cnt,i+1))
+                    cnt=0
+        
+        if cnt>=M-1:
+            delete_list += list(range(i+1-cnt,i+1+1))
+            cnt=0
+        
+        if not delete_list:
+            break
+        
+        new_arr=[]
+        for i in range(len(arr)):
+            if i in delete_list:
+                continue
+            new_arr.append(arr[i])
+        
+        arr = new_arr
+                 
+            
     print(len(arr))
     if arr:
         for a in arr:
