ec3c4a5b82682c988b4195a947440a75f5c89499
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-22 14:48:39 +0900

    Title: 1차원 폭발 게임, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,16 @@
+import sys
+
+
+if __name__=="__main__":
+    N,M=map(int, input().split())
+    arr=[]
+    for i in range(N):
+        boom=int(input())
+        if arr:
+            if arr[-1] == boom:
+                arr.pop()
+            else:
+                arr.append(boom)
+        else:
+            arr.append(boom)
+    print(len(arr))
