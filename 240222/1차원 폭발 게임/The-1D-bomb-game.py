2dd225092c19e2dbe7faf70331710e52a3f34545
Hyun Woo Kim <51473705+khw11044@users.noreply.github.com>
2024-02-22 19:10:36 +0900

    Title: 1차원 폭발 게임, Time: 187ms, Memory: 25MB, Status: Passed - CodeTree

--- 240222/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240222/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,37 +1,43 @@
 import sys
 
+def count_explode_bomb(start, num):
+    for end in range(start + 1, len(arr)):  # 현재위치 다음 위치부터 끝까지 
+        if arr[end] != num:
+            return end - 1
+        
+    return len(arr) - 1
+
 if __name__=="__main__":
-    N,M=map(int, input().split())
-    arr = []
-    for _ in range(N):
-        arr.append(int(input()))
-    
-    while arr:
-        delete_list=[]
-        cnt=1
-        for i in range(1,len(arr)):
-            if arr[i-1]==arr[i]:
-                cnt += 1
-            else:
-                if cnt>=M:
-                    delete_list+=list(range(i-cnt,i))
-                    cnt=1
+    # 변수 선언 및 입력
+    n, m = tuple(map(int, input().split()))
+    arr = [int(input()) for _ in range(n)]
+
+    while True:
+        did_explode = False
         
-        if cnt>=M:
-            delete_list+=list(range(len(arr)-cnt,len(arr)))
-        
-        if delete_list:
-            new_arr=[]
-            for j in range(len(arr)):
-                if j in delete_list:
-                    continue
-                new_arr.append(arr[j])
+        for start, num in enumerate(arr):
+            if num == 0:
+                continue
+
+            end = count_explode_bomb(start, num)
             
-            arr = new_arr
-        else:
-            break
-    
-    print(len(arr))
-    if arr:
+            if (end-start+1) >= m:
+                arr[start:end + 1] = [0] * (end - start + 1)    # 터질 폭탄의 num을 0으로 만듬 
+                did_explode = True
+            
+        arr = list(filter(lambda x: x > 0, arr))
+        '''
+        또는 
+        new_arr=[]
         for a in arr:
-            print(a)
+            if a!=0:
+                new_arr.append(a)
+        arr=new_arr
+        '''
+        
+        if not did_explode:     # 리스트 한바퀴 돌면서 터진적이 없으면 더이상 터질게 없으므로 while문 탈출
+            break
+
+    print(len(arr))
+    for num in arr:
+        print(num)
